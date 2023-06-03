479c98a10daadbd037a554d42e970da6085cdec7
dohametwally510 <70580494+dohametwally510@users.noreply.github.com>
2023-06-03 14:12:47 +0300

    Add files via upload

---
+++ data_models.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class SamplePostRequest(BaseModel):
+    a: int
+    b: str
+    c: list[int]
+    f: Optional[str]
---
+++ test.sh
@@ -0,0 +1,1 @@
+python -m pytest -vv test_hello.py
